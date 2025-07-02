f62c167e20ac481fbf92d911028693e7f76c355a
vishalsudhakar04 <vishalsvr2004@gmail.com>
2025-07-02 17:02:34 +0530

    Add Week 1 exercises: Singleton and Factory Method

---
+++ Week 1/FactoryMethodPatternExample/FactoryMethodPatternExample/Program.cs
@@ -0,0 +1,86 @@
+﻿public interface IDocument
+{
+    void Open();
+    void Close();
+    void Save();
+}
+
+public class WordDocument : IDocument
+{
+    public void Open() => Console.WriteLine("Opening Word document...");
+    public void Close() => Console.WriteLine("Closing Word document...");
+    public void Save() => Console.WriteLine("Saving Word document...");
+}
+
+public class PdfDocument : IDocument
+{
+    public void Open() => Console.WriteLine("Opening PDF document...");
+    public void Close() => Console.WriteLine("Closing PDF document...");
+    public void Save() => Console.WriteLine("Saving PDF document...");
+}
+
+public class ExcelDocument : IDocument
+{
+    public void Open() => Console.WriteLine("Opening Excel document...");
+    public void Close() => Console.WriteLine("Closing Excel document...");
+    public void Save() => Console.WriteLine("Saving Excel document...");
+}
+
+public abstract class DocumentFactory
+{
+    public abstract IDocument CreateDocument();
+
+    public void ManageDocument()
+    {
+        IDocument doc = CreateDocument();
+        doc.Open();
+        doc.Save();
+        doc.Close();
+    }
+}
+
+public class WordDocumentFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new WordDocument();
+    }
+}
+
+public class PdfDocumentFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new PdfDocument();
+    }
+}
+
+public class ExcelDocumentFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new ExcelDocument();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Console.WriteLine("Client: Using the WordDocumentFactory.");
+        wordFactory.ManageDocument();
+
+        Console.WriteLine();
+
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Console.WriteLine("Client: Using the PdfDocumentFactory.");
+        pdfFactory.ManageDocument();
+
+        Console.WriteLine();
+
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Console.WriteLine("Client: Using the ExcelDocumentFactory.");
+        excelFactory.ManageDocument();
+    }
+}
---
+++ Week 1/SingletonPatternExample/SingletonPatternExample/Program.cs
@@ -0,0 +1,44 @@
+﻿public sealed class Logger
+{
+    private static Logger _instance = null;
+
+    private Logger()
+    {
+        Console.WriteLine("Logger instance created.");
+    }
+
+    public static Logger GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Logger();
+        }
+        return _instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine($"Log message: {message}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger logger1 = Logger.GetInstance();
+        Logger logger2 = Logger.GetInstance();
+
+        logger1.Log("First message.");
+        logger2.Log("Second message.");
+
+        if (logger1 == logger2)
+        {
+            Console.WriteLine("Both variables hold the same instance. Singleton pattern works!");
+        }
+        else
+        {
+            Console.WriteLine("Singleton pattern failed.");
+        }
+    }
+}
