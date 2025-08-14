02cfd568d3c7d5f98d081d834e4c67df05ae63ac
vishalsudhakar04 <vishalsvr2004@gmail.com>
2025-08-14 20:38:38 +0530

    Add Week 6 React Exercise 2: StudentApp

---
+++ Week 6/studentapp/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ Week 6/studentapp/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h3>Welcome to the Home Page of Student Management Portal</h3>
+    </div>
+  );
+}
+
+export default Home;
